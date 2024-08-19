c10b8d4f9c716fb5cb37ad9567b239f25e70ff80
efeurhobopatricia <169153471+efeurhobopatricia@users.noreply.github.com>
2024-08-19 23:54:40 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,19 +2,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "263717750825"
-global.ownername = "Precious Ayomide👑"
+global.ownernomer = "2348078582627"
+global.ownername = "Only_one_🥇Empire"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '2348078582627'  //creator number
+global.ownername = 'Only_one_🥇Empire' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'PREXZY👑\n\nContact: +263717750825'
+global.author = 'Only_one_🥇Empire'
 
 //console view/theme
 global.themeemoji = '🪀'
@@ -57,7 +57,7 @@ global.mess = {
     owner: 'This feature can be used by owner only',
     group: 'This feature is only for groups',
     private: 'This feature is only for private chats',
-    wait: 'In process... ',    
+    wait: 'Demon is at work 😈🕷️',    
     error: 'Error!',
 }
 
